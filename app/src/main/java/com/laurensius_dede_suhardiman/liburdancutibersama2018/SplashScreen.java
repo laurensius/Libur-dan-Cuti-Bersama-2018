1715e1cb97c45fd908d4f676e914123dbda053c8
Laurensius Dede Suhardiman <burglar.tea@gmail.com>
2017-10-08 21:14:26 +0700

    Test Layout

--- app/src/main/java/com/laurensius_dede_suhardiman/liburdancutibersama2018/SplashScreen.java
+++ app/src/main/java/com/laurensius_dede_suhardiman/liburdancutibersama2018/SplashScreen.java
@@ -1,13 +1,32 @@
 package com.laurensius_dede_suhardiman.liburdancutibersama2018;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class SplashScreen extends AppCompatActivity {
+public class SplashScreen extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent i = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        timer.start();
     }
 }
